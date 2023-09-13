3e090a6b7a730c4e7037bdce057921892438ecb2
f1nnie2210 <longnguyen03072001@gmail.com>
2023-09-13 15:48:46 +0700

    read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,15 +1,15 @@
 const Course = require("../models/Course");
+const { multipleMongooseToObject } = require("../../util/mongoose");
 class SiteController {
   //GET /
-  async index(req, res) {
-    try {
-      const data = await Course.find({});
-      if (data) {
-        res.json(data);
-      }
-    } catch (error) {
-      res.status(400).json({ error: "ERROR!" });
-    }
+  async index(req, res, next) {
+    Course.find({})
+      .then((courses) => {
+        res.render("home", {
+          courses: multipleMongooseToObject(courses),
+        });
+      })
+      .catch(next);
   }
 
   //GET /search
