6b3ff38dedb600cf49c48ceae5049481d7f5f0d1
f1nnie2210 <longnguyen03072001@gmail.com>
2023-09-14 11:17:18 +0700

    create doc

--- src/app/controllers/CourseConTroller.js
+++ src/app/controllers/CourseConTroller.js
@@ -10,6 +10,21 @@ class CourseController {
       )
       .catch(next);
   }
+
+  //Create /courses/create
+  create(req, res, next) {
+    res.render("courses/create");
+  }
+  //POST /courses/store
+  store(req, res, next) {
+    const fromData = req.body;
+    fromData.image = `https://i.ytimg.com/vi/${req.body.videoId}/hq720.jpg`;
+    const course = new Course(fromData);
+    course
+      .save()
+      .then(() => res.redirect("/"))
+      .catch((error) => {});
+  }
 }
 
 module.exports = new CourseController();
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -3,6 +3,8 @@ const routes = express.Router();
 
 const courseController = require("../app/controllers/CourseConTroller");
 
+routes.get("/create", courseController.create);
+routes.post("/store", courseController.store);
 routes.get("/:slug", courseController.show);
 
 module.exports = routes;
